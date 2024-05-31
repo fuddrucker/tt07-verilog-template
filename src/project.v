75fe8bac153ce12a1c0abe5ac4171f6285ff3c21
fuddrucker <jd2murray@gmail.com>
2024-05-30 21:25:34 -0700

    Update project.v to implement 6 bit addr

--- src/project.v
+++ src/project.v
@@ -17,11 +17,10 @@ module tt_um_6bitaddr (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
   assign uio_oe  = 0;
+  assign {uo_out[6], uo_out[5:0]}  = ui_in[5:0] + uio_in[5:0] + ui_in[6];  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out[7] = 0;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
-
 endmodule
